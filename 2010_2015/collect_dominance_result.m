7f3d69772764193b14f3e541ae0d51c6e9185e80
David Gunawan <93757643+davidgun1000@users.noreply.github.com>
2025-05-23 14:03:54 +1000

    Add files via upload

---
+++ 2010_2015/collect_dominance_result.m
@@ -0,0 +1,102 @@
+%load the results
+
+load('U1_2010_2015.mat');
+load('U2_U3_result_2010_2015.mat');
+
+%collect dominance result
+
+mean(overall_xFSDy_income)%Table4
+mean(overall_yFSDx_income)%Table4
+mean(overall_xSSDy_income)%Table4
+mean(overall_ySSDx_income)%Table4
+
+mean(overall_xFSDy_health)%Table4
+mean(overall_yFSDx_health)%Table4
+mean(overall_xSSDy_health)%Table4
+mean(overall_ySSDx_health)%Table4
+
+mean(overall_xFSDy_educ)%Table4
+mean(overall_yFSDx_educ)%Table4
+mean(overall_xSSDy_educ)%Table4
+mean(overall_ySSDx_educ)%Table4
+
+mean(overall_xFSDy_happiness)%Table4
+mean(overall_yFSDx_happiness)%Table4
+mean(overall_xSSDy_happiness)%Table4
+mean(overall_ySSDx_happiness)%Table4
+
+mean(overall_xFSDy_income_lowest)%Table 6
+mean(overall_yFSDx_income_lowest)%Table 6
+mean(overall_xSSDy_income_lowest)%Table 6
+mean(overall_ySSDx_income_lowest)%Table 6
+
+mean(overall_xFSDy_health_lowest)%Table 6
+mean(overall_yFSDx_health_lowest)%Table 6
+mean(overall_xSSDy_health_lowest)%Table 6
+mean(overall_ySSDx_health_lowest)%Table 6
+
+mean(overall_xFSDy_educ_lowest)%Table 6
+mean(overall_yFSDx_educ_lowest)%Table 6
+mean(overall_xSSDy_educ_lowest)%Table 6
+mean(overall_ySSDx_educ_lowest)%Table 6
+
+mean(overall_xFSDy_happiness_lowest)%Table 6
+mean(overall_yFSDx_happiness_lowest)%Table 6
+mean(overall_xSSDy_happiness_lowest)%Table 6
+mean(overall_ySSDx_happiness_lowest)%Table 6
+
+overall_xFSDy_U1%last paragraph in Section 5.2
+overall_yFSDx_U1%last paragraph in Section 5.2
+overall_xFSDy_U1_intersection%Table 8
+overall_yFSDx_U1_intersection%Table 8
+
+mean(overall_xFSDy_U2)%Section 5.3
+mean(overall_yFSDx_U2)%Section 5.3
+mean(overall_xFSDy_income_health)%Table 5
+mean(overall_yFSDx_income_health)%Table 5
+mean(overall_xFSDy_income_educ)%Table 5
+mean(overall_yFSDx_income_educ)%Table 5
+mean(overall_xFSDy_income_happiness)%Table 5
+mean(overall_yFSDx_income_happiness)%Table 5
+
+mean(overall_xFSDy_U2_intersection)%Table 8
+mean(overall_yFSDx_U2_intersection)%Table 8
+mean(overall_xFSDy_income_health_intersection)%Table 7
+mean(overall_yFSDx_income_health_intersection)%Table 7
+mean(overall_xFSDy_income_educ_intersection)%Table 7
+mean(overall_yFSDx_income_educ_intersection)%Table 7
+mean(overall_xFSDy_income_happiness_intersection)%Table 7
+mean(overall_yFSDx_income_happiness_intersection)%Table 7
+
+mean(overall_xSSDy_income_health)%Table5
+mean(overall_ySSDx_income_health)%Table5
+mean(overall_xSSDy_income_educ)%Table5
+mean(overall_ySSDx_income_educ)%Table5
+mean(overall_xSSDy_income_happiness)%Table5
+mean(overall_ySSDx_income_happiness)%Table5
+mean(overall_xSSDy_income_health_compensated)%Table5
+mean(overall_ySSDx_income_health_compensated)%Table5
+mean(overall_xSSDy_income_educ_compensated)%Table5
+mean(overall_ySSDx_income_educ_compensated)%Table5
+mean(overall_xSSDy_income_happiness_compensated)%Table5
+mean(overall_ySSDx_income_happiness_compensated)%Table5
+mean(overall_xSSDy_income_health_educ_happiness_compensated)%Section 5.4
+mean(overall_ySSDx_income_health_educ_happiness_compensated)%Section 5.4
+
+mean(overall_xSSDy_income_health_intersection)%Table 7
+mean(overall_ySSDx_income_health_intersection)%Table 7
+mean(overall_xSSDy_income_educ_intersection)%Table 7
+mean(overall_ySSDx_income_educ_intersection)%Table 7
+mean(overall_xSSDy_income_happiness_intersection)%Table 7
+mean(overall_ySSDx_income_happiness_intersection)%Table 7
+mean(overall_xSSDy_income_health_compensated_intersection)%Table 7
+mean(overall_ySSDx_income_health_compensated_intersection)%Table 7
+mean(overall_xSSDy_income_educ_compensated_intersection)%Table 7
+mean(overall_ySSDx_income_educ_compensated_intersection)%Table 7
+mean(overall_xSSDy_income_happiness_compensated_intersection)%Table 7
+mean(overall_ySSDx_income_happiness_compensated_intersection)%Table 7
+mean(overall_xSSDy_income_health_educ_happiness_compensated_lowest)%Table 8
+mean(overall_ySSDx_income_health_educ_happiness_compensated_lowest)%Table 8
+
+
+
