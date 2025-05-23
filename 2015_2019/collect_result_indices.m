e76952829268e5cf6ffa7b66ec5ddc6c2fc63172
David Gunawan <93757643+davidgun1000@users.noreply.github.com>
2025-05-23 21:09:05 +1000

    Add files via upload

---
+++ 2015_2019/collect_result_indices.m
@@ -0,0 +1,63 @@
+load('indices_result_2015_2019.mat');
+
+load('param2015.mat');
+mean_education_x = prop_educ(:,1)*1 + prop_educ(:,2)*2 + prop_educ(:,3)*3 + prop_educ(:,4)*4 + prop_educ(:,5)*5;
+mean_happiness_x = prop_happiness(:,1)*1 + prop_happiness(:,2)*2 + prop_happiness(:,3)*3 + prop_happiness(:,4)*4 + prop_happiness(:,5)*5;
+
+load('param2019.mat');
+mean_education_y = prop_educ(:,1)*1 + prop_educ(:,2)*2 + prop_educ(:,3)*3 + prop_educ(:,4)*4 + prop_educ(:,5)*5;
+mean_happiness_y = prop_happiness(:,1)*1 + prop_happiness(:,2)*2 + prop_happiness(:,3)*3 + prop_happiness(:,4)*4 + prop_happiness(:,5)*5;
+
+
+
+mean(mean_income_y) - mean(mean_income_x)%Table 4
+mean(mean_health_y) - mean(mean_health_x)%Table 4
+mean(mean_education_y) - mean(mean_education_x)%Table 4
+mean(mean_happiness_y) - mean(mean_happiness_x)%Table 4
+
+
+load('MWI_2015.mat');
+MWI_income_health_x = MWI_income_health;
+MWI_income_education_x = MWI_income_education;
+MWI_income_happiness_x = MWI_income_happiness; 
+biv_headcount_income_health_x = biv_headcount_income_health;
+biv_headcount_income_education_x = biv_headcount_income_education;
+biv_headcount_income_happiness_x = biv_headcount_income_happiness;
+
+
+load('MWI_2019.mat');
+MWI_income_health_y = MWI_income_health;
+MWI_income_education_y = MWI_income_education;
+MWI_income_happiness_y = MWI_income_happiness; 
+biv_headcount_income_health_y = biv_headcount_income_health;
+biv_headcount_income_education_y = biv_headcount_income_education;
+biv_headcount_income_happiness_y = biv_headcount_income_happiness;
+
+
+
+mean(MWI_income_health_y) - mean(MWI_income_health_x)%Table 5
+mean(MWI_income_education_y) - mean(MWI_income_education_x)%Table 5
+mean(MWI_income_happiness_y) - mean(MWI_income_happiness_x)%Table 5
+
+
+mean(headcount_income_x) - mean(headcount_income_y)%Table 6
+mean(headcount_health_x) - mean(headcount_health_y)%Table 6
+mean(headcount_educ_x) - mean(headcount_educ_y)%Table 6
+mean(headcount_happiness_x) - mean(headcount_happiness_y)%Table 6
+
+mean(biv_headcount_income_health_x) - mean(biv_headcount_income_health_y)%Table7
+mean(biv_headcount_income_education_x) - mean(biv_headcount_income_education_y)%Table7
+mean(biv_headcount_income_happiness_x) - mean(biv_headcount_income_happiness_y)%Table7
+
+mean(multi_headcount_k1_x) - mean(multi_headcount_k1_y)%Table8
+mean(multi_headcount_k2_x) - mean(multi_headcount_k2_y)%Table8
+mean(multi_headcount_k3_x) - mean(multi_headcount_k3_y)%Table8
+mean(multi_headcount_k4_x) - mean(multi_headcount_k4_y)%Table8
+
+mean(multi_M0_k1_x) - mean(multi_M0_k1_y)%Table8
+mean(multi_M0_k2_x) - mean(multi_M0_k2_y)%Table8
+mean(multi_M0_k3_x) - mean(multi_M0_k3_y)%Table8
+mean(multi_M0_k4_x) - mean(multi_M0_k4_y)%Table8
+
+
+
---
+++ 2015_2019/plotting_draws.m
@@ -0,0 +1,73 @@
+
+load('param2015.mat');
+subplot(2,5,1);plot(prop_educ(:,1),'LineWidth',2.5);title('prop category 1 (education)');
+subplot(2,5,2);plot(prop_educ(:,2),'LineWidth',2.5);title('prop category 2 (education)');
+subplot(2,5,3);plot(prop_educ(:,3),'LineWidth',2.5);title('prop category 3 (education)');
+subplot(2,5,4);plot(prop_educ(:,4),'LineWidth',2.5);title('prop category 4 (education)');
+subplot(2,5,5);plot(prop_educ(:,5),'LineWidth',2.5);title('prop category 5 (education)');
+subplot(2,5,6);plot(prop_happiness(:,1),'LineWidth',2.5);title('prop category 1 (happiness)');
+subplot(2,5,7);plot(prop_happiness(:,2),'LineWidth',2.5);title('prop category 2 (happiness)');
+subplot(2,5,8);plot(prop_happiness(:,3),'LineWidth',2.5);title('prop category 3 (happiness)');
+subplot(2,5,9);plot(prop_happiness(:,4),'LineWidth',2.5);title('prop category 4 (happiness)');
+subplot(2,5,10);plot(prop_happiness(:,5),'LineWidth',2.5);title('prop category 5 (happiness)');
+
+subplot(2,3,1);plot(theta_gauss(:,1),'LineWidth',2);title('r12');
+subplot(2,3,2);plot(theta_gauss(:,2),'LineWidth',2);title('r13');
+subplot(2,3,3);plot(theta_gauss(:,3),'LineWidth',2);title('r14');
+subplot(2,3,4);plot(theta_gauss(:,4),'LineWidth',2);title('r23');
+subplot(2,3,5);plot(theta_gauss(:,5),'LineWidth',2);title('r24');
+subplot(2,3,6);plot(theta_gauss(:,6),'LineWidth',2);title('r34');
+
+subplot(2,3,1);plot(w_gamma(:,1),'LineWidth',2);title('\xi_{1G}');
+subplot(2,3,2);plot(w_gamma(:,2),'LineWidth',2);title('\xi_{2G}');
+subplot(2,3,3);plot(m_gamma(:,1),'LineWidth',2);title('\mu_{1}');
+subplot(2,3,4);plot(m_gamma(:,2),'LineWidth',2);title('\mu_{2}');
+subplot(2,3,5);plot(v_gamma(:,1),'LineWidth',2);title('v_{1}');
+subplot(2,3,6);plot(v_gamma(:,2),'LineWidth',2);title('v_{2}');
+
+subplot(3,3,1);plot(w_beta(:,1),'LineWidth',2);title('\xi_{1B}');
+subplot(3,3,2);plot(w_beta(:,2),'LineWidth',2);title('\xi_{2B}');
+subplot(3,3,3);plot(w_beta(:,3),'LineWidth',2);title('\xi_{3B}');
+subplot(3,3,4);plot(m_beta(:,1),'LineWidth',2);title('m_{1}');
+subplot(3,3,5);plot(m_beta(:,2),'LineWidth',2);title('m_{2}');
+subplot(3,3,6);plot(m_beta(:,3),'LineWidth',2);title('m_{3}');
+subplot(3,3,7);plot(s_beta(:,1),'LineWidth',2);title('s_{1}');
+subplot(3,3,8);plot(s_beta(:,2),'LineWidth',2);title('s_{2}');
+subplot(3,3,9);plot(s_beta(:,3),'LineWidth',2);title('s_{3}');
+
+
+load('param2019.mat');
+subplot(2,5,1);plot(prop_educ(:,1),'LineWidth',2.5);title('prop category 1 (education)');
+subplot(2,5,2);plot(prop_educ(:,2),'LineWidth',2.5);title('prop category 2 (education)');
+subplot(2,5,3);plot(prop_educ(:,3),'LineWidth',2.5);title('prop category 3 (education)');
+subplot(2,5,4);plot(prop_educ(:,4),'LineWidth',2.5);title('prop category 4 (education)');
+subplot(2,5,5);plot(prop_educ(:,5),'LineWidth',2.5);title('prop category 5 (education)');
+subplot(2,5,6);plot(prop_happiness(:,1),'LineWidth',2.5);title('prop category 1 (happiness)');
+subplot(2,5,7);plot(prop_happiness(:,2),'LineWidth',2.5);title('prop category 2 (happiness)');
+subplot(2,5,8);plot(prop_happiness(:,3),'LineWidth',2.5);title('prop category 3 (happiness)');
+subplot(2,5,9);plot(prop_happiness(:,4),'LineWidth',2.5);title('prop category 4 (happiness)');
+subplot(2,5,10);plot(prop_happiness(:,5),'LineWidth',2.5);title('prop category 5 (happiness)');
+
+subplot(2,3,1);plot(theta_gauss(:,1),'LineWidth',2);title('r12');
+subplot(2,3,2);plot(theta_gauss(:,2),'LineWidth',2);title('r13');
+subplot(2,3,3);plot(theta_gauss(:,3),'LineWidth',2);title('r14');
+subplot(2,3,4);plot(theta_gauss(:,4),'LineWidth',2);title('r23');
+subplot(2,3,5);plot(theta_gauss(:,5),'LineWidth',2);title('r24');
+subplot(2,3,6);plot(theta_gauss(:,6),'LineWidth',2);title('r34');
+
+subplot(2,3,1);plot(w_gamma(:,1),'LineWidth',2);title('\xi_{1G}');
+subplot(2,3,2);plot(w_gamma(:,2),'LineWidth',2);title('\xi_{2G}');
+subplot(2,3,3);plot(m_gamma(:,1),'LineWidth',2);title('\mu_{1}');
+subplot(2,3,4);plot(m_gamma(:,2),'LineWidth',2);title('\mu_{2}');
+subplot(2,3,5);plot(v_gamma(:,1),'LineWidth',2);title('v_{1}');
+subplot(2,3,6);plot(v_gamma(:,2),'LineWidth',2);title('v_{2}');
+
+subplot(3,3,1);plot(w_beta(:,1),'LineWidth',2);title('\xi_{1B}');
+subplot(3,3,2);plot(w_beta(:,2),'LineWidth',2);title('\xi_{2B}');
+subplot(3,3,3);plot(w_beta(:,3),'LineWidth',2);title('\xi_{3B}');
+subplot(3,3,4);plot(m_beta(:,1),'LineWidth',2);title('m_{1}');
+subplot(3,3,5);plot(m_beta(:,2),'LineWidth',2);title('m_{2}');
+subplot(3,3,6);plot(m_beta(:,3),'LineWidth',2);title('m_{3}');
+subplot(3,3,7);plot(s_beta(:,1),'LineWidth',2);title('s_{1}');
+subplot(3,3,8);plot(s_beta(:,2),'LineWidth',2);title('s_{2}');
+subplot(3,3,9);plot(s_beta(:,3),'LineWidth',2);title('s_{3}');
